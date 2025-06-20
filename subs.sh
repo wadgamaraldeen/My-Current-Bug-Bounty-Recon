443a53ce2e3ca8f064a518f73d09ca2b97b358d3
Mustafa Adam Qamar El-Din <71082772+wadgamaraldeen@users.noreply.github.com>
2025-06-20 12:25:24 +0200

    Create subs.sh

---
+++ subs.sh
@@ -0,0 +1,23 @@
+#!/bash/bin
+
+for url in $(cat domains.txt); do
+
+subfinder -d $url -all >> subfinder.txt;
+
+amass enum -passive -norecursive -noalts -d $url >> amass.txt;
+
+echo $url | assetfinder --subs-only >> asset.txt;
+
+python github-subdomains.py -t your-github-api-key-value -d $url | grep -v '@' | sort | grep "\.$url" >> github-subs.txt
+
+ curl -s https://crt.sh/?q=%25.$url | grep $url | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort >> crt.txt
+
+done
+
+ cat subfinder.txt amass.txt asset.txt crt.txt | anew all-subs.txt
+
+ rm -r subfinder.txt amass.txt asset.txt crt.txt
+
+ cat all-subs.txt | httpx -o live-subs.txt
+
+ #rm -r all-subs.txt
