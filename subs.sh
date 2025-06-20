0a21210e5d18cd6196aee2c54cfe663c655c0a70
Mustafa Adam Qamar El-Din <71082772+wadgamaraldeen@users.noreply.github.com>
2025-06-20 13:11:47 +0200

    Update subs.sh

--- subs.sh
+++ subs.sh
@@ -8,9 +8,9 @@ amass enum -passive -norecursive -noalts -d $url >> amass.txt;
 
 echo $url | assetfinder --subs-only >> asset.txt;
 
-python github-subdomains.py -t your-github-api-key-value -d $url | grep -v '@' | sort | grep "\.$url" >> github-subs.txt
+python github-subdomains.py -t your-github-api-key-value -d $url | grep -v '@' | sort -u | grep "\.$url" >> github-subs.txt
 
- curl -s https://crt.sh/?q=%25.$url | grep $url | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort >> crt.txt
+ curl -s https://crt.sh/?q=%25.$url | grep $url | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort -u >> crt.txt
 
 done
 
